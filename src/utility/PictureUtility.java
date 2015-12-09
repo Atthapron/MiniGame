619f9af932250eb1828ac4e06fc8844fba420638
5730679121-AP <qwertygms@hotmail.com>
2015-12-09 15:22:48 +0700

    add picture Utility(Unfinished)

---
+++ src/utility/PictureUtility.java
@@ -0,0 +1,10 @@
+package utility;
+
+import java.awt.image.BufferedImage;
+
+public class PictureUtility {
+	private static BufferedImage[] terrain;
+	private static BufferedImage[] shooter;
+	private static BufferedImage[] monster;
+	private static BufferedImage bar;
+}
