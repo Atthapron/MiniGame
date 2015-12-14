d14dd4000a2f59faef8fda36150121c730233c4e
5730679121-AP <qwertygms@hotmail.com>
2015-12-14 13:35:41 +0700

    update InputUtility

--- src/input/InputUtility.java
+++ src/input/InputUtility.java
@@ -1,10 +1,13 @@
 package input;
 
+
 public class InputUtility {
 	public static int mouseX,mouseY;
 	public static boolean mouseOnScreen = true;
 	private static boolean isLeftDown = false;
 	private static boolean isLeftClickedLastTick = false;
+	public static String type = ""; 
+	
 	
 	public static void mouseLeftDown(){
 		isLeftDown = true;
@@ -14,6 +17,9 @@ public class InputUtility {
 	public static void mouseLeftRelease(){
 		isLeftDown = false;
 	}
+	public static void setType(String type){
+		InputUtility.type = type;
+	}
 	
 	public static boolean isLeftClickTriggered(){
 		return isLeftClickedLastTick;
@@ -22,5 +28,9 @@ public class InputUtility {
 	public static void updateInputState(){
 		isLeftClickedLastTick = false;
 	}
+	public static void updateType(){
+		InputUtility.type = "";
+	}
+	
 
 }
